b84f429c2e06f67ec6dd88c735e23d63e2cb5b03
jpcoderesearch <jpcoderesearch@gmail.com>
2017-07-23 18:33:14 +0530

    change database secret to example.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,8 @@ set :deploy_to, '/home/deploy/gasol'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
