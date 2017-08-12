7ef9e060f83963aadff0570dd386c0de03b9ed7a
jpcoderesearch <jpcoderesearch@gmail.com>
2017-08-12 23:16:44 +0530

    add logo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '80.211.234.54', user: 'deploy', roles: %w{app db web}
+server '212.237.24.234', user: 'deploy', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
